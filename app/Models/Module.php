ce60321d1388a533933456a4ba1cc0ea8eb00944
Diego Sampaio <diego.estaleiro@gmail.com>
2023-06-26 21:05:24 -0300

    Adicionando módulo de cursos

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -6,17 +6,17 @@ use App\Traits\UuidTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Course extends Model
+class Module extends Model
 {
     use HasFactory, UuidTrait;
 
     public $incrementing = false;
     protected $keyType = 'uuid';
+    protected $fillable = ['name'];
 
-    protected $fillable = ['name', 'descrition', 'image'];
-
-    public function modules()
+    public function course()
     {
-        return $this->hasMany(Module::class);
+        return $this->belongsTo(Course::class);
     }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\{
-    CourseController
+    CourseController,
+    ModuleController
 };
 
 Route::get('/courses', [CourseController::class, 'index']);
 Route::get('/courses/{id}', [CourseController::class, 'show']);
+
+Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
 
 
 Route::get('/', function() {
