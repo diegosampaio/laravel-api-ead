6bd2e37ea728ff90e11d1c83e00adc447eb40533
Diego Sampaio <diego.estaleiro@gmail.com>
2023-06-26 21:42:39 -0300

    factories de modulos e aulas

--- app/Http/Resources/ModuleResource.php
+++ app/Http/Resources/ModuleResource.php
@@ -15,7 +15,7 @@ class ModuleResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'is' => $this->id,
+            'id' => $this->id,
             'name' => $this->name
         ];
     }
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ Route::get('/courses/{id}', [CourseController::class, 'show']);
 
 Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
 
-Route::get('/courses/{id}/lessons', [LessonController::class, 'index']);
+Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
 Route::get('/lessons/{id}', [LessonController::class, 'show']);
 
 
